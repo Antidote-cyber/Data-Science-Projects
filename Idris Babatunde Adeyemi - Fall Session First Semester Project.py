7a448a9d3675b1037aa8e68cda637b742fb6f006
Antidote-cyber <55806427+Antidote-cyber@users.noreply.github.com>
2023-12-18 15:38:55 +0100

    Added $ sign

--- Idris Babatunde Adeyemi - Fall Session First Semester Project.py
+++ Idris Babatunde Adeyemi - Fall Session First Semester Project.py
@@ -62,22 +62,22 @@ def main():
                 morning_shift_sales = list(map(float, input("Enter morning shift sales as a list (comma-separated): ").split(',')))
                 evening_shift_sales = list(map(float, input("Enter evening shift sales as a list (comma-separated): ").split(',')))
                 result = calculate_total_sales_for_the_day(morning_shift_sales, evening_shift_sales)
-                print(f'The total sales for the day is: {result}')
+                print(f'The total sales for the day is: ${result}')
                 
             elif choice == '2':
                 hourly_rate = float(input("Enter hourly rate: "))
                 hours_worked = float(input("Enter hours worked: "))
                 result = calculate_worker_salary(hourly_rate, hours_worked)
-                print(f'The worker salary is: {result}')
+                print(f'The worker salary is: ${result}')
                 
             elif choice == '3':
                 total_sales = sum(list(map(float, input("Enter sales of items as a list (comma-separated): ").split(','))))
                 total_cost = sum(list(map(float, input("Enter cost of items as a list (comma-separated): ").split(','))))
                 result = calculate_profit_or_loss(total_sales, total_cost)
                 if result < 0:
-                    print('LOSS: ',result)
+                    print('LOSS: $',result)
                 else:
-                    print('PROFIT: ',result)
+                    print('PROFIT: $',result)
                     
             elif choice == '4':
                 shift = input("Which shift do you want to calculate tips for? (Morning/Evening)").lower()
@@ -85,19 +85,19 @@ def main():
                 if shift[0] == 'm':
                     morning_shift_sales = list(map(float, input("Enter morning shift sales as a list (comma-separated): ").split(',')))
                     result = calculate_tips(morning_shift_sales)
-                    print(f"The tip for the morning shift is: {result}")
+                    print(f"The tip for the morning shift is: ${result}")
                 elif shift[0] == 'e':
                     evening_shift_sales = list(map(float, input("Enter evening shift sales as a list (comma-separated): ").split(',')))
                     result = calculate_tips(evening_shift_sales)
-                    print(f"The tip for the evening shift is: {result}")
+                    print(f"The tip for the evening shift is: ${result}")
                 else:
-                    print("Please specify the shift!")
+                    print("Please specify the shift! (Morning/Evening)")
                 
             elif choice == '5':
                 morning_shift_sales = list(map(float, input("Enter morning shift sales as a list (comma-separated): ").split(',')))
                 evening_shift_sales = list(map(float, input("Enter evening shift sales as a list (comma-separated): ").split(',')))
                 result = calculate_total_tips(morning_shift_sales, evening_shift_sales)
-                print(f'The total tips for the day is: {result}')
+                print(f'The total tips for the day is: ${result}')
                 
             else:
                 print("Invalid choice. Please enter a number between 1 and 6.")
